9e82331551714e47012d724a2869df7c428dcd4a
daisy-cao <31779733+daisy-cao@users.noreply.github.com>
2017-11-03 11:45:07 -0400

    Update and rename code for exercise1.py to exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -1,6 +1,17 @@
 import cv2
 
 img=cv2.imread('C:\Users\caoyu\Desktop\lenna.png')
+b,g,r=cv2.split(img)
+cv2.imshow('b',b)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imshow('g',g)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imshow('r',r)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
 img1=cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 img2=cv2.cvtColor(img,cv2.COLOR_BGR2YCrCb)
 #cv2.imshow('img1',img1)
