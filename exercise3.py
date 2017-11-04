b348fc109a451067e3d4848ce4c410f0f8134f30
daisy-cao <31779733+daisy-cao@users.noreply.github.com>
2017-11-04 00:00:58 -0400

    Update exercise3.py

--- exercise3.py
+++ exercise3.py
@@ -1,9 +1,10 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 img=cv2.imread('C:\Users\caoyu\Desktop\lenna.png',0)
 a,b=img.shape
-m=1
+k=1
 plt.figure(figsize=(10,10))
 for mean in (0,5,10,20):
     for sigma in (0,20,50,100):
@@ -13,5 +14,19 @@ for mean in (0,5,10,20):
         for i in range(a):
             for j in range(b):
                 temp[i][j]+=gauss[i][j]
-        plt.subplot(4,4,m),plt.imshow(img,'Greys')
-        m+=1
+        plt.subplot(4,4,k),plt.imshow(img,'Greys_r')
+        k+=1
+temp=img
+m=1
+for pb in (0.01,0.03,0.05,0.4):
+    for i in range(a):
+        for j in range(b):
+            rdn = random.random()
+            if rdn < pb:
+                temp[i][j] = 0
+            elif rdn > (1-pb):
+                temp[i][j] = 255
+            else:
+                temp[i][j] = img[i][j]
+    plt.subplot(2,2,m),plt.imshow(temp,'Greys_r')
+    m+=1
